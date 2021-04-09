21b9459887e830bb5038b830bd59261a43742f70
Ayler <liyale@bixin.cn>
2021-04-09 15:18:32 +0800

    init project

---
+++ Player/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by liyale on 2021/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ffmpeg")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .padding()
+            
+            
+    }
+}
---
+++ Player/Shared/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Shared
+//
+//  Created by liyale on 2021/3/29.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
